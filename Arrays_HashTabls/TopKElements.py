cedf8ea4e34e3fc625cf499fdb3cf16992cbe0e2
Sweet_Sempai <chcolatemilk00@gmail.com>
2025-03-15 18:40:29 +0300

    Решил Top K Frequent Elements - 50% сам, 50% с помощью подсказок - но мне совсем не нравиться решение, слишком много циклов

---
+++ Arrays_HashTabls/TopKElements.py
@@ -0,0 +1,24 @@
+"""
+### Top K Frequent Elements
+## Given an integer array nums and an integer k, return the k most frequent elements. You may return the answer in any order.
+https://leetcode.com/problems/top-k-frequent-elements/
+"""
+from collections import defaultdict
+from typing import List
+
+
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int):
+        result = []
+        counts = defaultdict(int)
+        freq = [[] for _ in range(len(nums) + 1)]
+        for num in nums:
+            counts[num] +=1
+        for key, value in counts.items():
+            freq[value].append(key)
+        for i in range(len(freq) - 1, 0, -1):
+            for num in freq[i]:
+                result.append(num)
+                if len(result) == k:
+                    return result
+        return result
---
+++ Tests/Arrays_HashTabls_tests/test_topkelements.py
@@ -0,0 +1,22 @@
+import pytest
+from Arrays_HashTabls.TopKElements import Solution
+
+
+@pytest.mark.parametrize(
+    "nums, k, expected",
+    [
+        ([1, 1, 1, 2, 2, 3], 2, [1,2]),
+        ([1], 1, [1]),
+        ([], 1, []),
+        ([1, 2, 2, 3], 1, [2]),
+    ],
+    ids=[
+        "basic_case",
+        "single_case",
+        "empty_case",
+        "collector_case"
+    ]
+)
+def test_topkelements(nums, k, expected):
+    solution = Solution()
+    assert solution.topKFrequent(nums, k) == expected
