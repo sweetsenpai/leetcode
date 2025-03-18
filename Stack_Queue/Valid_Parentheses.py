7148d89d98018a3cf051da6174ccc63a5e89c0df
Sweet_Sempai <chcolatemilk00@gmail.com>
2025-03-18 14:53:27 +0300

    Написал свое неправильное решение для Valid Parentheses

---
+++ Stack_Queue/Valid_Parentheses.py
@@ -0,0 +1,36 @@
+"""
+Valid Parentheses
+Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+An input string is valid if:
+
+    Open brackets must be closed by the same type of brackets.
+    Open brackets must be closed in the correct order.
+    Every close bracket has a corresponding open bracket of the same type.
+
+https://leetcode.com/problems/valid-parentheses/description/
+"""
+from collections import defaultdict
+
+
+class Solution:
+    @staticmethod
+    def isValid(s: str) -> bool:
+        breckit_dict = defaultdict(lambda: None)
+        breckit_dict["("] = ")"
+        breckit_dict["["] = "]"
+        breckit_dict["{"] = "}"
+        lenth = len(s)
+        if lenth % 2 !=0:
+            return False
+        s_list = list(s)
+        if not breckit_dict[s_list[0]]:
+            return False
+        for index, letter in enumerate(s_list):
+            if breckit_dict[letter]:
+                opposite_brecket = breckit_dict[letter]
+                if s_list[index+1]!= opposite_brecket and s_list[len(s) - 1 - index]!=opposite_brecket:
+                    return False
+
+        return True
+
---
+++ Tests/Stack_Queue_tests/test_valid_parantheses.py
@@ -0,0 +1,34 @@
+import pytest
+from Stack_Queue.Valid_Parentheses import Solution
+
+
+@pytest.mark.parametrize(
+    "s, expected",
+    [
+        ("()", True),
+        ("()[]{}", True),
+        ("(]", False),
+        ("([])", True),
+        ("([[()]])", True),
+        ("([[()]]", False),
+        ("{[(])}", False),
+        ("(]", False),
+        ("){", False),
+        ("(){}}{", False)
+    ],
+    ids=[
+        "easy_case_one_pair",
+        "easy_case_three_pair",
+        "easy_case_none_pair",
+        "easy_case_two_pair",
+        "hard_case_four_pair",
+        "hard_case_four_pair_false",
+        "chat_gpt_case_false",
+        "chat_gpt_second_case",
+        "leetcode_only_close",
+        "leet_code_error",
+    ]
+)
+def test_valid_parentheses(s, expected):
+    solution = Solution()
+    assert solution.isValid(s) == expected
