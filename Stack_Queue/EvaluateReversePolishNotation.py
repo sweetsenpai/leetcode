78551f08bc1954bf3e05e2c4ffad16e0bb07262c
Sweet_Sempai <chcolatemilk00@gmail.com>
2025-03-19 15:35:19 +0300

    Решил задачу Evaluate Reverse Polish notation

---
+++ Stack_Queue/EvaluateReversePolishNotation.py
@@ -0,0 +1,35 @@
+"""
+Evaluate Reverse Polish notation
+You are given an array of strings tokens that represents an arithmetic expression in a Reverse Polish Notation.
+
+Evaluate the expression. Return an integer that represents the value of the expression.
+
+Note that:
+
+    The valid operators are '+', '-', '*', and '/'.
+    Each operand may be an integer or another expression.
+    The division between two integers always truncates toward zero.
+    There will not be any division by zero.
+    The input represents a valid arithmetic expression in a reverse polish notation.
+    The answer and all the intermediate calculations can be represented in a 32-bit integer.
+
+https://leetcode.com/problems/evaluate-reverse-polish-notation/description/
+"""
+from typing import List
+import operator
+
+
+class Solution:
+    def evalRPN(self, tokens: List[str]) -> int:
+        operator_dict = {'+': lambda x, y: x + y,
+                         '-': lambda x, y: y - x,
+                         '*': lambda x, y: x * y,
+                         '/': lambda x, y: int(y / x)}
+        operand_list = []
+        for token in tokens:
+            if token in operator_dict.keys():
+                result = operator_dict[token](operand_list.pop(), operand_list.pop())
+                operand_list.append(result)
+            else:
+                operand_list.append(int(token))
+        return operand_list[0]
---
+++ Tests/Stack_Queue_tests/EvaluateReversePolishNotation_test.py
@@ -0,0 +1,21 @@
+import pytest
+from Stack_Queue.EvaluateReversePolishNotation import Solution
+
+
+@pytest.mark.parametrize(
+    "tokens, expected",
+    [
+        (["2", "1", "+", "3", "*"], 9),
+        (["4", "13", "5", "/", "+"], 6),
+        (["10", "6", "9", "3", "+", "-11", "*", "/", "*", "17", "+", "5", "+"], 22)
+    ],
+
+    ids=[
+        "leetcode_first_example",
+        "leetcode_second_example",
+        "leetcode_third_example"
+    ]
+)
+def test_erpn(tokens, expected):
+    solution = Solution()
+    assert solution.evalRPN(tokens) == expected
