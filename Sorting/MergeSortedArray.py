8ad33b07e7764d30fe12f6220ac3fe89c0b3467d
Sweet_Sempai <chcolatemilk00@gmail.com>
2025-03-29 18:20:15 +0300

    Обновил отчет

--- Sorting/MergeSortedArray.py
+++ Sorting/MergeSortedArray.py
@@ -14,19 +14,15 @@ from typing import List
 
 class Solution:
     def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
-        i, j, k = m - 1, n - 1, m + n - 1  # Указатели
-
-        while i >= 0 and j >= 0:  # Пока есть элементы в обоих массивах
-            if nums1[i] > nums2[j]:
-                nums1[k] = nums1[i]
+        p1 = m -1
+        p2 = n - 1
+        i = m + n - 1
+        while p2 >= 0:
+            if p1 >=0 and nums1[p1] > nums2[p2]:
+                nums1[i] = nums1[p1]
                 i -= 1
+                p1 -= 1
             else:
-                nums1[k] = nums2[j]
-                j -= 1
-            k -= 1  # Двигаем указатель на следующую позицию
-
-        # Если остались элементы в nums2, добавляем их (nums1 уже на месте)
-        while j >= 0:
-            nums1[k] = nums2[j]
-            j -= 1
-            k -= 1
+                nums1[i] = nums2[p2]
+                i -= 1
+                p2 -= 1
