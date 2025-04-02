016ed99723d300fc49cfb81004c5484c19a16624
Sweet_Sempai <chcolatemilk00@gmail.com>
2025-04-02 13:43:53 +0300

    Valid Palindrome - немного подправил решение - написал отчет

--- Sorting_Pointers/ValidPalindrome.py
+++ Sorting_Pointers/ValidPalindrome.py
@@ -7,17 +7,13 @@ Given a string s, return true if it is a palindrome, or false otherwise.
 
 https://leetcode.com/problems/valid-palindrome/
 """
-import re
 
 
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        s = re.sub(r'[^a-zA-Z0-9]', '', s).lower()
-
-        right = len(s)-1
+        s = ''.join(filter(str.isalnum, s)).lower()
 
         for left in range(len(s)//2):
-            if s[left] != s[right]:
+            if s[left] != s[len(s) - 1 - left]:
                 return False
-            right -= 1
         return True
